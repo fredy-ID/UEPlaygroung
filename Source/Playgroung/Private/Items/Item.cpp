a0942b429b19526193eb5dafb718d028ea6b448c
Fredy ID <56205156+fredy-ID@users.noreply.github.com>
2025-07-24 23:31:50 +0400

    Exposed properties (continuous learning)
    I exposed TimeConstant, Amplitude and RunningTime properties. I learned how initialize them with c++ is 3 variable initialisation ways. Then Eddited Amplitude and TimeConstant while RunningTime was private and read only

--- Source/Playgroung/Private/Items/Item.cpp
+++ Source/Playgroung/Private/Items/Item.cpp
@@ -17,11 +17,16 @@
 
 // Sets default values
 // A in AItem stands for "Actor," which is a base class for all objects that can be placed in the world.
-AItem::AItem()
+
+//Constructors in Unreal Engine are special functions that are called when an object of a class is created.
+AItem::AItem() : TimeConstant(5.f) // : Amplitude(0.25f) -> Initializing Amplitude in Constructor initialization list. This is one of the 3 ways to initialize a variable in C++ : constructor initialization list, constructor body, or in the class declaration
 {
+
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	// This is another way to initialize a variable in C++. but it is better to initialize variables in the constructor initialization list or in the class declaration.
+	//Amplitude = 0.25f; 
 }
 
 // Called when the game starts or when spawned
@@ -116,6 +121,7 @@ void AItem::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+	/* // This code is commented out, but it shows how to move and rotate the actor based on DeltaTime.
 	// Movement rate in units of centimeters per second (cm/s)
 	float MovementRate = 100.f;
 	float RotationRate = 50.f;
@@ -125,6 +131,16 @@ void AItem::Tick(float DeltaTime)
 	// MovementRate * DeltaTime (cm/s) * (s/frame) = cm/frame
 	AddActorWorldOffset(FVector(MovementRate * DeltaTime, 0.f, 0.f));
 	AddActorWorldRotation(FRotator(0.f, RotationRate * DeltaTime, 0.f));
+	*/
+
+	RunningTime += DeltaTime;
+
+	// This line shows how to use FMath::Sin to calculate a sine wave value.
+	// 0.25f is a multiplier to control the amplitude of the sine wave wich is the height of the wave*
+	// Sin function takes an angle in radians, so we use RunningTime as the input to create a smooth oscillation over time.
+	float DeltaZ = Amplitude * FMath::Sin(RunningTime * TimeConstant); // period = 2* PI / K where K is the frequency of the sine wave (all the way up and all the way down). In this case, K = 5.f, so the period is 2*PI/5.f
+	AddActorWorldOffset(FVector(0.f, 0.f, DeltaZ));
+
 	DRAW_DEBUG_SPHERE_SingleFrame(GetActorLocation());
 	DRAW_DEBUG_VECTOR_SingleFrame(GetActorLocation(), GetActorLocation() + GetActorForwardVector() * 100.f);
 
--- Source/Playgroung/Public/Items/Item.h
+++ Source/Playgroung/Public/Items/Item.h
@@ -19,6 +19,34 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Sine Parameters (Custom C++)")
+	float Amplitude = 0.25f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Sine Parameters (Custom C++)")
+	float TimeConstant;
+
+private:
+	// To access a private property, we use meta modifier. Allows the property to be visible in the editor and read-only in Blueprints but we can still access with meta AllowPrivateAccess.
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	float RunningTime;
+
+	// Can only be edited in the game instance, not in the editor.
+	//UPROPERTY(EditInstanceOnly)
+
+
+	// Amplitude initialization in the class declaration. This is one of the 3 ways to initialize a variable in C++ : constructor initialization list, constructor body, or in the class declaration
+	
+	// This is a property that can be edited in the Unreal Engine editor and is accessible in Blueprints.
+	// EditAnywhere allows the property to be edited in the editor, BlueprintReadWrite allows it to be read and written in Blueprints.
+	// EditDefaultsOnly would restrict editing to the defaults only, and BlueprintReadOnly would make it read-only in Blueprints.
+	// UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item Properties", meta = (AllowPrivateAccess = "true"))
+	
+	// Can only be edited in the editor, not in the game instance.
+	//UPROPERTY(EditDefaultsOnly)
+
+	
+
+
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
